a2868a22c536c230d3016945a85d8cf81c0ac940
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-08-04 19:21:54 -0700

    added single thought query and modal

--- server/server.js
+++ server/server.js
@@ -49,6 +49,10 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 })
+
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, './public/404.html'));
+})
 };
 
 // call the async function to start the server
